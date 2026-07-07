62646f140db2ae975c1f8668f954481d13fbe315
Aysha Nuha <ayshan8971@gmail.com>
2026-07-07 12:08:35 +0530

    backend/settings.py databases changed

--- backend/settings.py
+++ backend/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'resume_analyzer',
-        'USER': 'root',
-        'PASSWORD': 'Laya@2003',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
